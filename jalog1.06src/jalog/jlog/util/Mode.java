c3143f257cbc5db79e355c18e4155a40aed04f4c
bsucs <122378640+bsucs@users.noreply.github.com>
2025-02-20 21:00:15 -0500

    updated file

--- jalog1.06src/jalog/jlog/util/Mode.java
+++ jalog1.06src/jalog/jlog/util/Mode.java
@@ -1,3 +1,34 @@
+
+/*
+ * JaLog - Amateur Radio Logging Software
+ * 
+ * Copyright (C) 2013 minex123
+ * Contributor: Ron Kinney (GitHub: septantrionalis) - 2019
+ * Update by: Campbell Reed - 2025
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * --- Modification Notice ---
+ * Changes made by Campbell Reed (2025):
+ * - Fixed a typo in the FM mode definition to ensure proper mode selection.
+ *
+ * This modification retains the original GPL licensing terms and ensures that
+ * the software remains free and open-source.
+ */
+
+
+
 package jalog.jlog.util;
 
 /**
